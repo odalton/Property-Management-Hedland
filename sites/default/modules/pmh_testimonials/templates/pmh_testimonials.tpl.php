56ba46710346ca52be0d692268a7ef85b5d89d6a
odalton <oliver.dalton.au@gmail.com>
2014-10-20 13:29:28 +0800

    Updated the layout based off client mockup. - Updated header removed border bottom. - Re aligned testimonial title. - reduced membership logo size. - updated links on social media icons - added link to copyright link. - added read more style to testimonial content type.

--- sites/default/modules/pmh_testimonials/templates/pmh_testimonials.tpl.php
+++ sites/default/modules/pmh_testimonials/templates/pmh_testimonials.tpl.php
@@ -2,9 +2,10 @@
   <?php
   foreach ($testimonials as $testimonial):
     print '<div class="slide">';
-    print '<div class="testimonial-body"><blockquote>';
+    print '<div class="testimonial-body"><blockquote><p>';
     print $testimonial['body'];
-    print '</blockquote></div>';
+    print '<br><a href="'.$testimonial['field_read_more_link'].'" target="_blank" title="Read More Testimonials">Read More</a></p></blockquote>';
+    print '</div>';
     print '<div class="testimonial-title">';
     print $testimonial['field_full_name'];
     print '</div>';
